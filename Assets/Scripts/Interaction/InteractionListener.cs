b593a7ce04d0b635448983fe4d34e227a2d74a15
anton04 <anton.gustafsson@ri.se>
2018-02-12 15:29:30 +0100

    test

--- Assets/Scripts/Interaction/InteractionListener.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface InteractionListener {
-	void OnTap(Vector3 position);
-	void OnSwipe(Vector2 direction);
-}
--- Assets/Scripts/Util/CallbackResult.cs
+++
@@ -1,6 +0,0 @@
-﻿
-public class CallbackResult {
-
-    public bool result;
-
-}
